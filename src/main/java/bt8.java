84f3d958e492024ced17c47f9dab099027bcae1f
baokhang-hub <mtbaokhang@gmail.com>
2026-08-24 14:17:43 +0700

    Bai tap 8

---
+++ src/main/java/bt8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class bt8 {
+    public static void main(String[] args) {
+        final int soBiMat = 9;
+        Scanner input = new Scanner(System.in);
+
+        int count = 0;
+        do {
+            System.out.println("Nhap so cua ban: ");
+            int number = input.nextInt();
+            count++;
+
+            if (number == soBiMat) {
+                break;
+            }
+        } while (true);
+        System.out.println("Chinh xac! Ban da doan dung sau " + count + " lan");
+    }
+}
